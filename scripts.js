bbae3af16ca3d9ba138975cb231bf47538cf706a
Josh Goebel <dreamer3@gmail.com>
2012-08-24 06:16:58 -0400

    looks like zepto fix is no longer needed

--- scripts.js
+++ scripts.js
@@ -7,7 +7,6 @@ var Bonfire={
 		if (Bonfire.initing) { return }
 		Bonfire.initing = true;
     // Textual.include_js("jquery.tiny.js");
-    // Textual.include_js("zepto.tiny.js");
     Textual.include_js("zepto8.tiny.js");
     Textual.include_js("support.js");
     // evidentally the page needs a second to render, parse JS, etc
@@ -16,26 +15,8 @@ var Bonfire={
 	start: function()
 	{
     // console.log("start");
-	  Bonfire.fixup_zepto();
 		Bonfire.render = new Renderer($("#thelog"), $("#body_home"));
 		Bonfire.started = true;
-	},
-	fixup_zepto: function()
-	{
-	  // have to hack a zepto function becase we are not an IOS device
-	  $.fn.offset = function(){
-	    var obj;
-      if(this.length==0) return null;
-      try {
-      obj = this[0].getBoundingClientRect();
-      } catch (e) { obj = { left:0, top: 0} }
-      return {
-        left: obj.left + document.body.scrollLeft,
-        top: obj.top + document.body.scrollTop,
-        width: obj.width,
-        height: obj.height
-      };
-    }
 	},
 };
 
