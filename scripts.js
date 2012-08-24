2bf8a37dbad035284cd18e30ab349a3a53ad8446
Josh Goebel <dreamer3@gmail.com>
2012-08-24 04:51:18 -0400

    almost there

--- scripts.js
+++ scripts.js
@@ -12,6 +12,7 @@ var Bonfire={
     // Textual.include_js("jquery.tiny.js");
     // Textual.include_js("zepto.tiny.js");
     Textual.include_js("zepto8.tiny.js");
+    Textual.include_js("support.js");
 		window.setTimeout(Bonfire.start, 25);
 	},
 	fixup_zepto: function()
@@ -35,158 +36,34 @@ var Bonfire={
 	{
 	  console.log("start");
 	  Bonfire.fixup_zepto();
-		Bonfire.dump=$("#body_home");
-		Bonfire.table=$("#thelog");
-		Bonfire.table.hide();
-		//remap body_home
-    // body_home.attr("id","container");
-    // Bonfire.table.attr("id","body_home");
 		Bonfire.started=true;
-		
-		// cap the link width based on the window size before we redraw
-    // Bonfire.cap_link_width();
-    Bonfire.redraw();
-		// recalculate the link width after resizing the window
-    // window.addEventListener("resize", Bonfire.cap_link_width);
-		// recalculate the link width after we hopefully have some content
-    // window.setTimeout(Bonfire.cap_link_width, 30000);
+		Bonfire.render = new Renderer($("#thelog"), $("#body_home"));
 	},
-	cap_link_width:function()
-	{
-		var width, column, style_fixes;
-		column=$("tr:first-child td",Bonfire.table).first();
-		// if we have any columns in the table, use those
-		if (column.length!=0) {
-			// use offsetWidth to avoid needing the full jquery library
-			width=$(window).width()-column[0].offsetWidth;
-			width=Math.ceil(width*0.85);
-		} else {
-			width=Math.ceil($(window).width()*0.6); }
-		// look for our fixes stylesheet
-		style_fixes=$("head style#fixes");
-		if (style_fixes.length==0) // if we can't find it then, create it
-			style_fixes=$("<style id='fixes'>").appendTo($("head"));		
-		css="table.bf td.msg a { max-width:" + width + "px; }\n";
-		css+="table.bf { max-width: " + $(window).width() + "px }";
-		style_fixes.html(css)
-	},
-	done_drawing: function()
-	{
-	  Bonfire.redrawing=false;
-	  Bonfire.table.show();
-	  Bonfire.cap_link_width();
-	  Textual.scrollToBottomOfView();
-	  // try it after we have some content
-	  window.setTimeout(Bonfire.cap_link_width, 30000);
-	  window.addEventListener("resize", Bonfire.cap_link_width);
-	},
-	redraw: function()
-	{
-	  Bonfire.redrawing=true;
-	  Bonfire.render_nothing = Bonfire.render_nothing || 0;
-	  console.log("redraw");
-    // console.log(Bonfire.dump.html());
-    var lines = Bonfire.dump.find("table tr.line");
-		lines.each (function(i) {
-		  Bonfire.redrawn += 1;
-			num=this.id.replace("line","");
-			num=parseInt(num);
-			Textual.newMessagePostedToView(num, true);
-		});
-		if (lines.length==0 && Bonfire.redrawn > 0)
-		  Bonfire.render_nothing += 1;
-		// keep looping till we've chewed thru the backlog
-		if (Bonfire.render_nothing <= 3){
-		  window.setTimeout(Bonfire.redraw, 50);
-		} else {
-		  Bonfire.done_drawing();
-		}
-	},
-	move_mark: function()
-	{
-		// look for the div mark
-		mark=$("#body_home div#mark");
-		if (mark.size() > 0)
-		{
-			mark.remove();
-			$("#mymark").remove();
-			// and create our own row mark
-			row=$("<tr>").attr("id","mymark");
-			col=$("<td colspan='2'></td>").appendTo(row);
-			Bonfire.table.append(row);
-		}
-	}
 };
 
-// render a time stamp every 5 minutes
-function render_time(time)
-{
-  var row;
-	var ts=new Date;
-	if (Bonfire.last_time)
-		diff=(ts-Bonfire.last_time)/1000/60; // minutes
-	// if a new window or haven't printed a timestamp in the past 5 minutes
-	if (Bonfire.last_time==null || diff>5)
-	{
-		row=$("<tr class='time'><td></td><td>" + time + "</td></tr>");
-		// nick doesn't count as a repeat if a timestamp separates it
-		Bonfire.last_nick=null;
-		Bonfire.table.append(row);
-		Bonfire.last_time=ts;
-	}
-}
 Textual.newMessagePostedToView=function(lineNumber,backlog)
 {
-  var row, time;
-  // window.console.log("new message posted:" + lineNumber);
-	if (!Bonfire.started) {
-	  window.console.warn("bonfire is not started yet")
-		Bonfire.init();
+  // FinishedLoding or FinishedReload event should spooling us up, patience
+	if (!Bonfire.started)
 		return;
-	}
-	// move the mark
-	if (!Bonfire.redrawing)
-		Bonfire.move_mark();
+	
+  // window.console.log("new message posted:",lineNumber,
+  //     "backlog:", backlog);
+
+  // wait until we are finishg redrawing to start normal logging
 	if (Bonfire.redrawing && !backlog)
 	  return;
   
-  row = Bonfire.dump.find("#line" + lineNumber);
-  if (row[0]==null) {
-    console.error("can not find line" + lineNumber );
-    setTimeout(Bonfire.redraw, 100);
-  }
-    
-  
-  // render time
-  time = row.find("span.time");
-  render_time(time.html());
-  time.remove();
-  
-  // hide same nick in a row
-  var sender = row.find("span.sender");
-	var nick=sender.html();
-	if (nick && nick!=Bonfire.last_nick) {
-      // sender[0].oncontextmenu = function() { Textual.on_nick() }; 
-			Bonfire.last_nick=nick;
-	} else {
-		sender.remove();
-	}
-	
-	// highlight our own rows
-	var p=row.find("p"); // this is where the myself class is set
-	if (p.attr("type")=="myself")
-		row.addClass("myself");
-		
-  // move the row
-  row.parent().parent().remove();
-  Bonfire.table.append(row);
-  // window.console.log("after move");
+  Bonfire.render.message(lineNumber);
+  return;
 }
 
 // replace Textual mark with our own
 Textual.historyIndicatorAddedToView = function()
 {
-  Bonfire.move_mark();
+  console.log("historyIndicatorAddedToView");
+  if (Bonfire.render)
+    Bonfire.render.move_mark();
 }
 
 Textual.viewFinishedLoading = function() 
---
+++ support.coffee
@@ -0,0 +1,112 @@
+class @Renderer
+  constructor: (@table, @input) ->
+    @table.hide()
+    @draw()
+  draw_done: (final) ->
+    @table.show()
+    Textual.scrollToBottomOfView()
+    @cap_link_width()
+    @setup_cap_links() if final
+  draw: ->
+    @drawing = true
+    @decay ||= 25
+    lines = @input.find("table tr.line")
+    lines.each (i, o) =>
+      num=o.id.replace("line","")
+      num=parseInt(num)
+      @message(num)
+    if lines.length > 0
+      @draw_done()
+    @decay *= 2
+    unless @decay > 6400
+      setTimeout () =>
+        @draw()
+      , @decay
+    else
+      @drawing = false
+      @draw_done(true)
+  
+  # support
+  setup_cap_links: ->
+    setTimeout @cap_link_width, 30000
+    window.addEventListener "resize", @cap_link_width
+  cap_link_width: -> 
+    `var width, column, style_fixes;
+    column=$("tr:first-child td",this.table).first();
+    // if we have any columns in the table, use those
+    if (column.length!=0) {
+      // use offsetWidth to avoid needing the full jquery library
+      width=$(window).width()-column[0].offsetWidth;
+      width=Math.ceil(width*0.85);
+    } else {
+      width=Math.ceil($(window).width()*0.6); }
+    if (width==0)
+      width = 200;
+    // look for our fixes stylesheet
+    style_fixes=$("head style#fixes");
+    if (style_fixes.length==0) // if we can't find it then, create it
+      style_fixes=$("<style id='fixes'>").appendTo($("head"));    
+    css="table.bf td.msg a { max-width:" + width + "px; }\n";
+    css+="table.bf { max-width: " + $(window).width() + "px }";
+    style_fixes.html(css)`
+    null
+    
+  # individual line type routines
+  time: (s) ->
+    ts = new Date
+    if @last_time
+      diff=(ts-@last_time)/1000/60; # minutes
+    # if a new window or haven't printed a timestamp in the past 5 minutes
+    if @last_time or diff > 5
+      row = $("<tr class='time'><td></td><td>" + time + "</td></tr>")
+      # nick doesn't count as a repeat if a timestamp separates it
+      Bonfire.last_nick = null
+      @table.append row
+      @last_time = ts
+  message: (lineNumber) ->
+    row = @input.find("#line#{lineNumber}")
+
+    # HACK - keep trying until we have it
+    unless row[0]
+      console.warn "missing #{lineNumber}, retrying"
+      setTimeout () =>
+        @message(lineNumber)
+      ,50
+      return
+      
+    # HACK - sometimes mark is not added to the DOM when historyIndicatorAddedToView is called
+    # we'll just try and remove it every time
+    @input.find("div#mark").remove();
+
+    # tr -> tbody -> table
+    row.parent().parent().remove()
+
+    # render time
+    time = row.find("span.time")
+    @time time.html()
+    # time.html("line#" +lineNumber);
+    time.remove()
+
+    # hide same nick in a row
+    sender = row.find("span.sender")
+    nick = sender.attr("nick")
+    if nick and nick != Bonfire.last_nick
+      Bonfire.last_nick = nick
+    else 
+      sender.remove()
+
+    @table.append row
+    # window.console.log("after move");
+    
+  move_mark: ->
+    # look for the div mark
+    mark = @input.find("div#mark")
+    if mark[0]==null
+      console.error "missing the mark"
+    if mark.length > 0 or 1==1
+      mark.remove()
+      $("#mymark").remove();
+      # and create our own row mark
+      row = $("<tr>").attr("id","mymark");
+      col = $("<td colspan='2'></td>").appendTo(row);
+      @table.append(row);
